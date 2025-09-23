05aa5cb00d044e3a77513d30fb0585d1279e5ec1
Winnie <49323767+Winnie0609@users.noreply.github.com>
2025-09-23 13:52:04 +0800

    Enhance ExerciseSelect component with search functionality using Fuse.js for improved exercise selection. Refactor WorkoutDashboard to accept exercise data as a prop and update session management logic to prepend new sessions. Adjust UI elements for better accessibility and user experience.

--- components/ExerciseSelect.tsx
+++ components/ExerciseSelect.tsx
@@ -1,4 +1,7 @@
-import { Check,ChevronsUpDown } from 'lucide-react';
+'use client';
+
+import Fuse from 'fuse.js';
+import { Check, ChevronsUpDown } from 'lucide-react';
 import { useState } from 'react';
 import { FieldValues, useFormContext } from 'react-hook-form';
 
@@ -19,17 +22,28 @@ import {
 import { ExerciseData } from '@/lib/types';
 import { cn } from '@/lib/utils';
 
+interface ExerciseSelectProps {
+  field: FieldValues;
+  exerciseData: ExerciseData[];
+  exIndex: number;
+}
+
+const fuseOptions = {
+  includeScore: true,
+  keys: ['titleEn', 'titleZh', 'aliases'],
+};
+
 export function ExerciseSelect({
   field,
   exerciseData,
   exIndex,
-}: {
-  field: FieldValues;
-  exerciseData: ExerciseData[];
-  exIndex: number;
-}) {
-  const [open, setOpen] = useState(false);
+}: ExerciseSelectProps) {
   const { setValue } = useFormContext();
+  const [open, setOpen] = useState(false);
+  const [searchResults, setSearchResults] =
+    useState<ExerciseData[]>(exerciseData);
+
+  const fuse = new Fuse(exerciseData, fuseOptions);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
@@ -39,11 +53,11 @@ export function ExerciseSelect({
           role="combobox"
           aria-expanded={open}
           className={cn(
-            `justify-between ${
-              field.value ? 'text-primary' : 'text-muted-foreground'
-            }`,
-            'w-full'
+            'w-full justify-between',
+            !field.value && 'text-muted-foreground'
           )}
+          ref={field.ref}
+          onBlur={field.onBlur}
         >
           {field.value
             ? exerciseData.find((exercise) => exercise.id === field.value)
@@ -52,35 +66,49 @@ export function ExerciseSelect({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-full p-0" side="bottom" align="start">
+      <PopoverContent
+        className="w-[--radix-popover-trigger-width] p-0"
+        side="bottom"
+        align="start"
+      >
         <Command>
-          <CommandInput placeholder="Search exercise" className="h-9" />
+          <CommandInput
+            placeholder="Search by name"
+            onValueChange={(query) => {
+              if (query.length > 0) {
+                const results = fuse.search(query).map((result) => result.item);
+                setSearchResults(results);
+              } else {
+                setSearchResults(exerciseData);
+              }
+            }}
+          />
           <CommandList>
             <CommandEmpty>No exercise found.</CommandEmpty>
             <CommandGroup>
-              {exerciseData.map((exercise) => (
+              {searchResults.map((exercise) => (
                 <CommandItem
+                  className="w-[300px] md:w-full"
                   key={exercise.id}
                   value={exercise.titleEn}
                   onSelect={() => {
+                    // set name
+                    setValue(`exercises.${exIndex}.name`, exercise.titleEn, {
+                      shouldValidate: true,
+                    });
                     field.onChange(exercise.id);
                     setOpen(false);
-                    setValue(
-                      `exercises.${exIndex}.name`,
-                      exercise.titleEn || '',
-                      {
-                        shouldValidate: true,
-                      }
-                    );
                   }}
                 >
-                  {exercise.titleEn}
                   <Check
                     className={cn(
-                      'ml-auto',
+                      'mr-2 h-4 w-4',
                       field.value === exercise.id ? 'opacity-100' : 'opacity-0'
                     )}
                   />
+                  <span className="truncate" title={exercise.titleEn}>
+                    {exercise.titleEn}
+                  </span>
                 </CommandItem>
               ))}
             </CommandGroup>
